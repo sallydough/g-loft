24ceeb91deb5395732dc26d92a7839e5472c097c
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-12-03 09:16:53 -0700

    add pop up contacts

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import Slider from "react-slick";
 import { FaArrowRight, FaArrowLeft, FaUserAlt } from "react-icons/fa";
-
-
+import Modal from 'react-modal';
 import './contacts.css'
 
 const navbar = [
@@ -36,21 +35,49 @@ const navbar = [
   },
 
 ];
+// Custom hook for handling the modal
+const useContactModal = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedContact, setSelectedContact] = useState(null);
+
+  const openModal = (contact) => {
+    setSelectedContact(contact);
+    setModalIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
+
+  return {
+    modalIsOpen,
+    selectedContact,
+    openModal,
+    closeModal,
+  };
+};
 
 function Contacts() {
-    //click contact to call
+  const { modalIsOpen, selectedContact, openModal, closeModal } = useContactModal();
+  const [cardIndex, setCardIndex] = useState(0);
+//call using window
   const contactCall = () => {
-    const phoneNumber = '+1234556778';
-    const userChoice = window.confirm('Do you want to call or send an SMS?');
-  
-  if (userChoice) {
-    window.location.href = `tel:${phoneNumber}`;
-  } else {
-    window.location.href = `sms:${phoneNumber}`;
-  }
-    const telUrl = `tel:${phoneNumber}`;
-    window.location.href = telUrl;
+   if (selectedContact) {
+      const phoneNumber = '+1234556778';
+      window.location.href = `tel:${phoneNumber}`;
+      closeModal(); // Close the modal after initiating the call
+    }
   };
+//   const userChoice = window.confirm('Do you want to call or send an SMS?');
+  
+  // if (userChoice) {
+  //   window.location.href = `tel:${phoneNumber}`;
+  // } else {
+  //   window.location.href = `sms:${phoneNumber}`;
+  // }
+  //   const telUrl = `tel:${phoneNumber}`;
+  //   window.location.href = telUrl;
+  // };
 
   
 const NextArrow = ({ onClick }) => {
@@ -68,9 +95,7 @@ const NextArrow = ({ onClick }) => {
       </div>
     );
   };
-
-  const [cardIndex, setCardIndex] = useState(0);
-
+//slides effect
   const slidesSettings = {
     infinite: true,
     lazyLoad: true,
@@ -83,18 +108,16 @@ const NextArrow = ({ onClick }) => {
     beforeChange: (current, next) => setCardIndex(next),
   };
 
-  
-
   return (
     <>
     <div id="lights" className="settings">
       <div className="nav-bar-logo">
-        <h1>Logo and mini nav bar</h1>
+        <h1>Back To Loft Controls</h1>
       </div>
       <div className="slider">
       <Slider className="linkStyle"  {...slidesSettings}>
         {navbar.map((card, idx) => (
-          <div id={card.id} onClick={contactCall} className={idx === cardIndex ? "slide activeSlide" : "slide"}   >
+          <div key={card.id} onClick={() => openModal(card)} className={idx === cardIndex ? "slide activeSlide" : "slide"}   >
              <h1>{card.name}</h1>
             {card.icon}
              </div>
@@ -102,8 +125,21 @@ const NextArrow = ({ onClick }) => {
       </Slider>
       </div>
     </div>
-
-    </>
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Call Modal"
+        className="custom-modal centered-modal"
+      >
+        {selectedContact && (
+          <div>
+            <h2>{`Do you want to call ${selectedContact.name}?`}</h2>
+            <button onClick={contactCall}>Yes</button>
+            <button onClick={closeModal}>No</button>
+          </div>
+        )}
+      </Modal>
+       </>
   );
 };
 
